3676fa9018b6c5c7dbc638cc0bead47c54d28dc7
dj1969 <dimitrijeromov@gmail.com>
2021-06-20 18:26:21 -0500

    part2 q2 finished

--- part2.js
+++ part2.js
@@ -1,19 +1,25 @@
 // Make a request to the Deck of Cards API to request a single card from a newly shuffled deck. Once you have the card, console.log the value and the suit (e.g. “5 of spades”, “queen of diamonds”).
 async function getDeckAndDraw1() {
-    //get new deck
-    let data = await $.getJSON('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
-    let deck_id = data.deck_id;
-
-    //draw one card
-    let data2 = await $.getJSON(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
-    let value = data2.cards[0].value;
-    let suit = data2.cards[0].suit;
-
+    let data = await $.getJSON(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`);
+    let {suit, value} = data.cards[0];
     console.log(`${value} of ${suit}`)
 }
 getDeckAndDraw1();
 
 // Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the same deck.
+async function getDeckAndDraw2() {
+    //two separate requests
+    let data = await $.getJSON(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`);
+    let deck_id = data.deck_id;
+    let {suit, value} = data.cards[0];
+    let card1 = `${value} of ${suit}`;
+    let data2 = await $.getJSON(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
+    let {suit: suit2, value: value2} = data2.cards[0];
+    let card2 = `${value2} of ${suit2}`;
+    console.log(card1, card2);
+}
+getDeckAndDraw2();
+
 
 // Once you have both cards, console.log the values and suits of both cards.
 
