c9bd83279f45ffa45bd7a15ec973a4c65777ad58
mits <dmitrybaycharov@gmail.com>
2021-06-21 09:08:46 -0500

    reluctantly finished pt2

--- part2.js
+++ part2.js
@@ -24,4 +24,31 @@ getDeckAndDraw2();
 
 // Build an HTML page that lets you draw cards from a deck. When the page loads, go to the Deck of Cards API to create a new deck, and show a button on the page that will let you draw a card. Every time you click the button, display a new card, until there are no cards left in the deck.
 
-
+$(function() {
+    let baseURL = 'https://deckofcardsapi.com/api/deck';
+    
+    // 3.
+    async function setup() {
+      let $btn = $('button');
+      let $cardArea = $('#card-area');
+  
+      let deckData = await $.getJSON(`${baseURL}/new/shuffle/`);
+      $btn.show().on('click', async function() { 
+        let cardData = await $.getJSON(`${baseURL}/${deckData.deck_id}/draw/`);
+        let cardSrc = cardData.cards[0].image;
+        let angle = Math.random() * 90 - 45;
+        let randomX = Math.random() * 40 - 20;
+        let randomY = Math.random() * 40 - 20;
+        $cardArea.append(
+          $('<img>', {
+            src: cardSrc,
+            css: {
+              transform: `translate(${randomX}px, ${randomY}px) rotate(${angle}deg)`
+            }
+          })
+        );
+        if (cardData.remaining === 0) $btn.remove();
+      });
+    }
+    setup();
+  });
